78e6b9deca30227f5a6e574d7f187bf914180e19
Kurt Lust <Kurt.Lust@uantwerpen.be>
2021-03-12 16:11:47 +0100

    Various improvments and corrections to the SofteareStack family modules for the CalcUA infrastructure.

--- Modules/SoftwareStack/CalcUA/2020a.lua
+++ Modules/SoftwareStack/CalcUA/2020a.lua
@@ -1,17 +1,26 @@
-local toolchain =    '2020a'
-local intelversion = '2020'
-local gccversion =   '9.3.0'
+--
+-- Some configuration
+-- This should also align with the EasyBuild configuration modules
+--
+
+-- Default directory for user EasyBuild-installed software
+local default_user_prefix = os.getenv( 'VSC_DATA' ) .. '/EasyBuild'
+-- Site-specific prefix for the environment variable names that are likely set either in the system profile or user profile.
+local ebu =  'EBU'
+-- Site-specific location of the modules
+local module_prefix = '/apps/antwerpen/modules/'
 
 -- -----------------------------------------------------------------------------
 --
--- The part below here is generic
+-- Generic part of the module file
 --
 
---
--- Some configuration that is the same for all toolchains
---
+local stack_version = myModuleVersion()
+local stack_name =    myModuleName()
 
-local module_prefix = '/apps/antwerpen/modules/'
+--
+-- Some configuration that is the same for all stack_versions
+--
 
 local procarch  = os.getenv('VSC_ARCH_LOCAL')
 local clusteros = os.getenv('VSC_OS_LOCAL')
@@ -22,16 +31,42 @@ if procarch == 'rome' then
 end
 
 --
+-- Read environment variables that may point to a user EasyBuild installation.
+--
+local user_prefix = os.getenv( ebu .. '_USER_PREFIX' )
+if ( user_prefix    == nil ) then user_prefix    = default_user_prefix    end
+
+local stack    = stack_name .. '-' .. stack_version
+local archspec = clusteros .. '-' .. archspec_target
+local user_installpath_modules =  pathJoin( user_prefix, 'stacks', stack, archspec, 'usermodules-' .. stack )
+
+--
 -- Information about the module (help information, family, etc)
 --
 
-local helpstring = string.gsub( string.gsub( string.gsub( [[
-This module enables all toolchain-independent and TCVER toolchains modules for users.
-The TCVER toolchains use the Intel INTELVER compilers and GNU compilers version GCCVER.
-]], 'TCVER', toolchain), 'INTELVER', intelversion), 'GCCVER', gccversion)
+local helpstring = string.gsub( [[
+Description
+===========
+This module enables the CalcUA/VERSION software stack which consists mostly out
+of software compiled with the intel/VERSION module or a compatible GNU compiler,
+and a selection of software packages installed from binaries.
+
+To get the precise versions of the compilers used, load the module and use
+module help intel and module help GCC to get the help information on the compiler
+modules.
+
+We try to keep a specific version of a software stack working for two years, though
+this may not always be possible as underlying changes in the OS may break
+compatibility with some applications. Problems have occured in the past and are likely
+to occur again (e.g., early versions of GCC 7 and all versions of the 2018 Intel
+compilers are not fully compatible with CentOS 8).
+]], 'VERSION', stack_version )
+
 help( helpstring )
 
-local whatisstring = string.gsub( 'Description: Enables toolchain-independent and TCVER modules', 'TCVER', toolchain )
+local whatisstring = string.gsub(
+    'Description: Enables the local VERSION software stack and a selection of packages installed from binaries',
+    'VERSION', stack_version )
 whatis( whatisstring )
 
 family('SoftwareStack')
@@ -40,31 +75,36 @@ family('SoftwareStack')
 -- Set a number of local non-VSC environment variables used to configure EasyBuild
 --
 
-setenv( 'CALCUA_STACK_VERSION',          toolchain )
-setenv( 'CALCUA_STACK_NAME',             'CalcUA' )
+setenv( 'CALCUA_STACK_VERSION',          stack_version )
+setenv( 'CALCUA_STACK_NAME',             stack_name )
 setenv( 'CALCUA_ARCHSPEC_ARCH',          'linux-' .. clusteros .. '-' .. archspec_target )
 setenv( 'CALCUA_ARCHSPEC_PLATFORM',      'linux' )
 setenv( 'CALCUA_ARCHSPEC_OS',            clusteros )
 setenv( 'CALCUA_ARCHSPEC_TARGET_COMPAT', procarch )
 setenv( 'CALCUA_ARCHSPEC_TARGET',        archspec_target )
-setenv( 'VSC_TARGET_LOCAL',              archspec_target )
+-- setenv( 'VSC_TARGET_LOCAL',           archspec_target )
 
 --
 -- Build the MODULEPATH
 --
 
--- Software stack supports EasyBuild
-prepend_path('MODULEPATH', '/data/antwerpen/202/vsc20259/EasyBuildDev/EasyBuildLocalMod/Modules/Manual')
+-- Make EasyBuild-user and EasyBuild-production available
+prepend_path( 'MODULEPATH', '/data/antwerpen/202/vsc20259/EasyBuildDev/EasyBuildLocalMod/Modules/Manual' )
 
--- Generic 64-bit software
-prepend_path('MODULEPATH', module_prefix .. clusteros .. '/software-x86_64')
+-- Generic 64-bit software (usually installed from binaries)
+prepend_path( 'MODULEPATH',   pathJoin( module_prefix, clusteros, 'software-x86_64' ) )
 
--- Toolchain-independent modules
-prepend_path('MODULEPATH', module_prefix .. clusteros .. '/software-' .. procarch .. '/system')
+-- Modules compiled with the system compiler and not yet part of an official software stack
+if not ( stack_version == 'system' ) then
+  prepend_path( 'MODULEPATH', pathJoin( module_prefix, clusteros, 'software-' .. procarch, 'system' ) )
+end
 
--- toolchain
-prepend_path('MODULEPATH', module_prefix .. clusteros .. '/software-' .. procarch .. '/' .. toolchain)
+-- The actual software of the software stack
+prepend_path( 'MODULEPATH',   pathJoin( module_prefix, clusteros, 'software-' .. procarch , stack_version ) )
+
+-- Do we have user modules installed? If so, load them.
+if isDir( user_installpath_modules ) then prepend_path( 'MODULEPATH', user_installpath_modules ) end
 
 --- List of deprecated modules
-setenv( 'LMOD_ADMIN_FILE', module_prefix .. 'lmod/admin.list' )
+setenv( 'LMOD_ADMIN_FILE', pathJoin( module_prefix, 'lmod/admin.list' ) )
 
--- Modules/SoftwareStack/generic-x86.lua
+++ Modules/SoftwareStack/generic-x86.lua
@@ -1,16 +1,45 @@
 --
--- Some configuration that is the same for all toolchains
+-- Some configuration
+-- This should also align with the EasyBuild configuration modules
 --
 
+-- Default directory for user EasyBuild-installed software
+local default_user_prefix = os.getenv( 'VSC_DATA' ) .. '/EasyBuild'
+-- Site-specific prefix for the environment variable names that are likely set either in the system profile or user profile.
+local ebu =  'EBU'
+-- Site-specific location of the modules
 local module_prefix = '/apps/antwerpen/modules/'
+
+-- -----------------------------------------------------------------------------
+--
+-- Generic part of the module file
+--
+
+local stack_version = ''
+local stack_name =    myModuleName()
+
+--
+-- Some configuration that is the same for all toolchains
+--
 
 local procarch  = os.getenv('VSC_ARCH_LOCAL')
 local clusteros = os.getenv('VSC_OS_LOCAL')
 
-local archspec_target = procarch
+local archspec_target_vsc = procarch
 if procarch == rome then
-    archspec_target = 'zen2'
+    archspec_target_vsc = 'zen2'
 end
+local archspec_target = 'x86_64'
+
+--
+-- Read environment variables that may point to a user EasyBuild installation.
+--
+local user_prefix = os.getenv( ebu .. '_USER_PREFIX' )
+if ( user_prefix    == nil ) then user_prefix    = default_user_prefix    end
+
+local stack    = stack_name
+local archspec = clusteros .. '-' .. archspec_target
+local user_installpath_modules =  pathJoin( user_prefix, 'stacks', stack, archspec, 'usermodules-' .. stack )
 
 --
 -- Information about the module (help information, family, etc)
@@ -32,24 +61,27 @@ family('SoftwareStack')
 -- Set a number of local non-VSC environment variables used to configure EasyBuild
 --
 
-setenv( 'CALCUA_STACK_VERSION',          'default' )
-setenv( 'CALCUA_STACK_NAME',             'generic_x86' )
+setenv( 'CALCUA_STACK_NAME',             'generic-x86' )
+setenv( 'CALCUA_STACK_VERSION',          '' )
 setenv( 'CALCUA_ARCHSPEC_ARCH',          'linux-' .. clusteros .. '-' .. archspec_target )
 setenv( 'CALCUA_ARCHSPEC_PLATFORM',      'linux' )
 setenv( 'CALCUA_ARCHSPEC_OS',            clusteros )
-setenv( 'CALCUA_ARCHSPEC_TARGET_COMPAT', 'x86_64' )
-setenv( 'CALCUA_ARCHSPEC_TARGET',        'x86_64' )
-setenv( 'VSC_TARGET_LOCAL',              archspec_target )
+setenv( 'CALCUA_ARCHSPEC_TARGET_COMPAT', archspec_target )
+setenv( 'CALCUA_ARCHSPEC_TARGET',        archspec_target )
+-- setenv( 'VSC_TARGET_LOCAL',           archspec_target_vsc )
 
 --
 -- Build the MODULEPATH
 --
 
--- Software stack supports EasyBuild
+-- Make EasyBuild-user and EasyBuild-production available
 prepend_path('MODULEPATH', '/data/antwerpen/202/vsc20259/EasyBuildDev/EasyBuildLocalMod/Modules/Manual')
 
 -- Generic 64-bit software
-prepend_path('MODULEPATH', module_prefix .. 'centos7/software-x86_64')
+prepend_path('MODULEPATH', pathJoin( module_prefix, clusteros, 'software-x86_64' ) )
+
+-- Do we have user modules installed? If so, load them.
+if isDir( user_installpath_modules ) then prepend_path( 'MODULEPATH', user_installpath_modules ) end
 
 --- List of deprecated modules
-setenv( 'LMOD_ADMIN_FILE', module_prefix .. 'lmod/admin.list' )
+setenv( 'LMOD_ADMIN_FILE', pathJoin( module_prefix, 'lmod/admin.list' ) )
